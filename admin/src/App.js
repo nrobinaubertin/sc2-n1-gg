1ed4553ddd8b044fa5f3a55a15c3e7f82f564fa7
Niels Robin-Aubertin <git@niels.fr>
2019-05-01 18:46:25 +0200

    Fix urls for the admin

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,4 +1,4 @@
 import React from 'react';
 import { HydraAdmin } from '@api-platform/admin';
 
-export default () => <HydraAdmin entrypoint="http://localhost/api"/>;
+export default () => <HydraAdmin entrypoint="/api"/>;
